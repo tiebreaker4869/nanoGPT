85444f7f0c5a8d1c56508c0ffd6580725391cdb9
Lifan Sun <lifansun1412@gmail.com>
2024-07-29 20:04:57 +0800

    implement GPT model

--- model.py
+++ model.py
@@ -113,3 +113,82 @@ class Block(nn.Module):
         x = x + self.attention(self.layernorm_1(x))
         x = x + self.mlp(self.layernorm_2(x))
         return x
+    
+class GPT(nn.Module):
+    """
+    The full GPT language model.
+    """
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        self.config = config
+        self.token_emb = nn.Embedding(config.vocab_size, config.n_embed)
+        self.positional_emb = nn.Embedding(config.block_size, config.n_embed)
+        self.dropout = nn.Dropout(config.dropout)
+        self.blocks = nn.ModuleList([Block(config) for _ in range(config.n_layer)])
+        self.layernorm_final = LayerNorm(config.n_embed, config.bias)
+        self.final_proj = nn.Linear(config.n_embed, config.vocab_size)
+
+        self.apply(self._init_weights)
+    
+    def forward(self, x: torch.Tensor, target: torch.Tensor = None) -> torch.Tensor:
+        # x: (B, T)
+        device = x.device
+        B, T = x.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        temb = self.token_emb(x)
+        pemb = self.positional_emb(torch.arange(0, T, dtype=torch.long, device=device).unsqueeze(0))
+        x = temb + pemb
+        x = self.dropout(x)
+        #(B, T, C)
+
+        for block in self.blocks:
+            x = block(x)
+        x = self.layernorm_final(x)
+
+        if target is not None:
+            logits = self.final_proj(x)
+            # logits: (B, T, vocab_size)
+            # target: (B, T)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), target.view(-1))
+        else:
+            # inference time optimization: only forward the logits in last position
+            logits = self.final_proj(x[:, [-1], :])
+            loss = None
+        
+        return logits, loss
+
+    @torch.no_grad()
+    def generate(self, idx: torch.Tensor, max_new_tokens: int, temperature: float = 1.0, top_k: int = None):
+        """
+        Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
+        the sequence max_new_tokens times, feeding the predictions back into the model each time.
+        Most likely you'll want to make sure to be in model.eval() mode of operation for this.
+        """
+        for _ in range(max_new_tokens):
+            # if the sequence context is growing too long we must crop it at block_size
+            idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
+            # forward the model to get the logits for the index in the sequence
+            logits, _ = self(idx_cond)
+            # pluck the logits at the final step and scale by desired temperature
+            logits = logits[:, -1, :] / temperature
+            # optionally crop the logits to only the top k options
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = -float('Inf')
+            # apply softmax to convert logits to (normalized) probabilities
+            probs = F.softmax(logits, dim=-1)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)
+            # append sampled index to the running sequence and continue
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
+
+
+    def _init_weights(self, module: nn.Module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
